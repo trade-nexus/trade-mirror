f498f7bd108d1bc13cf640177292e6c2d31bde07
Umer Aziz Malik <umer@aurorasolutions.org>
2012-12-27 19:15:04 +0500

    + Update

--- AutoFXToolsTradeMirror/service/DBHelper.cs
+++ AutoFXToolsTradeMirror/service/DBHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MySql.Data.MySqlClient;
-using System.Collections.Concurrent;
 using TraceSourceLogger;
 
 namespace Microsoft.Samples.NetTcp
