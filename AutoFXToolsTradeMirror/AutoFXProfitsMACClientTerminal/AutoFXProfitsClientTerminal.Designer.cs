6fab95f3cf60f0528bb521fae004d9004fe7961e
Umer Aziz Malik <umer@aurorasolutions.org>
2013-01-16 16:07:41 +0500

    + Client Terminal for MAC

---
+++ AutoFXToolsTradeMirror/AutoFXProfitsMACClientTerminal/AutoFXProfitsClientTerminal.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace AutoFXProfitsMACClientTerminal
+{
+    partial class AutoFXProfitsClientTerminal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AccountTextBox = new System.Windows.Forms.TextBox();
+            this.KeyStringTextBox = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.DisconnectButton = new System.Windows.Forms.Button();
+            this.SaveKeyStringCheckBox = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName/Account No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key String:";
+            // 
+            // AccountTextBox
+            // 
+            this.AccountTextBox.Location = new System.Drawing.Point(169, 23);
+            this.AccountTextBox.Name = "AccountTextBox";
+            this.AccountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AccountTextBox.TabIndex = 2;
+            // 
+            // KeyStringTextBox
+            // 
+            this.KeyStringTextBox.Location = new System.Drawing.Point(169, 65);
+            this.KeyStringTextBox.Name = "KeyStringTextBox";
+            this.KeyStringTextBox.Size = new System.Drawing.Size(100, 20);
+            this.KeyStringTextBox.TabIndex = 3;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ConnectButton.Location = new System.Drawing.Point(66, 105);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = false;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButtonClick);
+            // 
+            // DisconnectButton
+            // 
+            this.DisconnectButton.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.DisconnectButton.Location = new System.Drawing.Point(160, 105);
+            this.DisconnectButton.Name = "DisconnectButton";
+            this.DisconnectButton.Size = new System.Drawing.Size(75, 23);
+            this.DisconnectButton.TabIndex = 5;
+            this.DisconnectButton.Text = "Disconnect";
+            this.DisconnectButton.UseVisualStyleBackColor = false;
+            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButtonClick);
+            // 
+            // SaveKeyStringCheckBox
+            // 
+            this.SaveKeyStringCheckBox.AutoSize = true;
+            this.SaveKeyStringCheckBox.Location = new System.Drawing.Point(93, 145);
+            this.SaveKeyStringCheckBox.Name = "SaveKeyStringCheckBox";
+            this.SaveKeyStringCheckBox.Size = new System.Drawing.Size(102, 17);
+            this.SaveKeyStringCheckBox.TabIndex = 6;
+            this.SaveKeyStringCheckBox.Text = "Save Key String";
+            this.SaveKeyStringCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(151, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Status:";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusLabel.Location = new System.Drawing.Point(191, 175);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(0, 19);
+            this.StatusLabel.TabIndex = 8;
+            // 
+            // AutoFXProfitsClientTerminal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(292, 198);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SaveKeyStringCheckBox);
+            this.Controls.Add(this.DisconnectButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.KeyStringTextBox);
+            this.Controls.Add(this.AccountTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "AutoFXProfitsClientTerminal";
+            this.Text = "Auto FX Profits Client Terminal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox AccountTextBox;
+        private System.Windows.Forms.TextBox KeyStringTextBox;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button DisconnectButton;
+        private System.Windows.Forms.CheckBox SaveKeyStringCheckBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label StatusLabel;
+    }
+}
+
---
+++ AutoFXToolsTradeMirror/AutoFXProfitsMACClientTerminal/AutoFXProfitsClientTerminal.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using System.Timers;
+using System.Windows.Forms;
+using TraceSourceLogger;
+using Color = System.Drawing.Color;
+using Timer = System.Timers.Timer;
+
+namespace AutoFXProfitsMACClientTerminal
+{
+    public partial class AutoFXProfitsClientTerminal : Form, ITradeMirrorCallback
+    {
+        private static readonly Type OType = typeof(AutoFXProfitsClientTerminal);
+
+        private readonly InstanceContext _site;
+        private TradeMirrorClient _client;
+
+        private static Timer _heartbeatTimer;
+        private const int AcceptedDelaySeconds = 60;
+
+        public string AccountID
+        {
+            get { return AccountTextBox.Text; } 
+            set { AccountTextBox.Text = value; }
+        }
+
+        public string KeyString
+        {
+            get { return KeyStringTextBox.Text; } 
+            set { KeyStringTextBox.Text = value; }
+        }
+
+        public string Status
+        {
+            get { return StatusLabel.Text; }
+            set { StatusLabel.Text = value; }
+        }
+
+        public bool IsSavePasswordChecked
+        {
+            get { return SaveKeyStringCheckBox.Checked; }
+            set { SaveKeyStringCheckBox.Checked = value; }
+        }
+
+        public Color Color
+        {
+            get { return StatusLabel.ForeColor; }
+            set { StatusLabel.ForeColor = value; }
+        }
+
+        public AutoFXProfitsClientTerminal()
+        {
+            try
+            {
+                InitializeComponent();
+
+                // Create a client
+                _site = new InstanceContext(null, this);
+                _client = new TradeMirrorClient(_site);
+
+                _heartbeatTimer = new Timer(AcceptedDelaySeconds * 1000);
+                _heartbeatTimer.Elapsed += HeartbeatTimerElapsed;
+                _heartbeatTimer.AutoReset = true;
+
+                UpdateUI("Disconnected");
+
+                if (!InitializeCredentials())
+                {
+                    SetAccountID();
+                }
+                else
+                {
+                    IsSavePasswordChecked = true;
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "AutoFXProfitsClientTerminal");
+            }
+        }
+
+        /// <summary>
+        /// Connect the client to the server
+        /// </summary>
+        public void ConnectToServer()
+        {
+            try
+            {
+                string suffixes = _client.Subscribe(AccountID, KeyString, Convert.ToInt32(AccountID));
+
+                if (suffixes != "FAILED")
+                {
+                    Logger.Info("Subscribed", OType.FullName, "ConnectToServer");
+
+                    UpdateUI("Connected");
+
+                    SetSuffixes(suffixes);
+
+                    _heartbeatTimer.Enabled = true;
+
+                    if (IsSavePasswordChecked)
+                    {
+                        SaveAccountCredentials();
+                    }
+                    else
+                    {
+                        DeleteAccountCredentials();
+                    }
+                }
+                else
+                {
+                    Logger.Info("Invalid Credentials", OType.FullName, "ConnectToServer");
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "ConnectToServer");
+            }
+        }
+
+        /// <summary>
+        /// Disconnect the client from the server
+        /// </summary>
+        public void DisconnectFromServer()
+        {
+            try
+            {
+                if (_client.Unsubscribe(AccountID, KeyString, Convert.ToInt32(AccountID)))
+                {
+                    Logger.Info("Unsubscribed", OType.FullName, "DisconnectFromServer");
+
+                    UpdateUI("Disconnected");
+                }
+                else
+                {
+                    Logger.Info("Invalid Credentials", OType.FullName, "DisconnectFromServer");
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "DisconnectFromServer");
+            }
+        }
+
+        /// <summary>
+        /// Method called when a new signal arrives
+        /// </summary>
+        /// <param name="signalInformation"></param>
+        public void NewSignal(string signalInformation)
+        {
+            if (signalInformation.Contains("___autofxtools trademirror___Alive___"))
+            {
+                Logger.Debug("Heartbeat. = " + signalInformation, OType.FullName, "NewSignal");
+                ResetTimer();
+                return;
+            }
+
+            Logger.Debug("New Signal Received = " + signalInformation, OType.FullName, "NewSignal");
+            PlaceOrder(signalInformation);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void HeartbeatTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            DisconnectFromServer();
+            Logger.Debug("Connection lost to server", OType.FullName, "HeartbeatTimerElapsed");
+        }
+
+        /// <summary>
+        /// Updates output on UI
+        /// </summary>
+        /// <param name="newStatus"></param>
+        private void UpdateUI(string newStatus)
+        {
+            try
+            {
+                this.Status = newStatus;
+
+                if (Status == "Connected")
+                {
+                    Color = Color.FromArgb(255, 0, 255, 0);
+                }
+                else if (Status == "Disconnected")
+                {
+                    Color = Color.FromArgb(255, 255, 0, 0);
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "UpdateUI");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetAccountID()
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + @"\\accountinfo.txt";
+
+                if (File.Exists(path))
+                {
+                    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+                    StreamReader streamReader = new StreamReader(fs);
+
+                    string tempString = string.Empty;
+
+                    while ((tempString = streamReader.ReadLine()) != null)
+                    {
+                        if (tempString.Contains("accountNumber"))
+                        {
+                            string[] tempArray = tempString.Split(':');
+                            AccountID = tempArray[1].Trim();
+                        }
+                        break;
+                    }
+
+                    streamReader.Close();
+                    fs.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "SetAccountID");
+            }
+        }
+
+        /// <summary>
+        /// Saves order into a text file
+        /// </summary>
+        public void PlaceOrder(string orderInfo)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\orders.csv";
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                using (File.Create(path))
+                {
+                }
+
+                var fileStream = new FileStream(path, FileMode.Open, FileAccess.Write, FileShare.None);
+                var streamWriter = new StreamWriter(fileStream);
+
+                streamWriter.Write(orderInfo);
+                Logger.Debug("Signal Information Written to file", OType.FullName, "PlaceOrder");
+
+                streamWriter.Close();
+                fileStream.Close();
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "PlaceOrder");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SaveAccountCredentials()
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + @"\\trademirrorcredentials.txt";
+
+                if (!File.Exists(path))
+                {
+                    FileStream fs = File.Create(path);
+                    StreamWriter streamWriter = new StreamWriter(fs);
+
+                    streamWriter.Write(AccountID + " : " + KeyString);
+                    Logger.Debug("User Credentials saved. Account ID = " + AccountID + " | Keystring = " + KeyString, OType.FullName, "PlaceOrder");
+
+                    streamWriter.Close();
+                    fs.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "SaveAccountCredentials");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void DeleteAccountCredentials()
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + @"\\trademirrorcredentials.txt";
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                Logger.Debug("User Credentials Deleted" + KeyString, OType.FullName, "SaveAccountCredentials");
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "SaveAccountCredentials");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private bool InitializeCredentials()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"\\trademirrorcredentials.txt";
+
+            if (File.Exists(path))
+            {
+                FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+                StreamReader streamReader = new StreamReader(fs);
+
+                string tempString = streamReader.ReadLine();
+
+                string[] tempArray = tempString.Split(':');
+
+                AccountID = tempArray[0].Trim();
+                KeyString = tempArray[1].Trim();
+
+                Logger.Debug("User Credentials Initialized. Account ID = " + AccountID + " | Keystring = " + KeyString, OType.FullName, "InitializeCredentials");
+
+                streamReader.Close();
+                fs.Close();
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void FreeResources()
+        {
+            if (Status == "Connected")
+            {
+                this.DisconnectFromServer();
+            }
+            if (_client != null)
+            {
+                _client.Close();
+                _client.Abort();
+                _client = null;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ResetTimer()
+        {
+            _heartbeatTimer.Stop();
+            _heartbeatTimer.Start();
+        }
+
+        private void SetSuffixes(string suffixes)
+        {
+            try
+            {
+                string path = AppDomain.CurrentDomain.BaseDirectory + "\\suffixes.csv";
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                using (File.Create(path))
+                {
+                }
+
+                FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Write, FileShare.None);
+                StreamWriter streamWriter = new StreamWriter(fileStream);
+
+                streamWriter.Write(suffixes);
+                Logger.Debug("System Suffixes = " + suffixes, OType.FullName, "SetSuffixes");
+                streamWriter.Write(";");
+
+                streamWriter.Close();
+                fileStream.Close();
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception, OType.FullName, "SetSuffixes");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ConnectButtonClick(object sender, EventArgs e)
+        {
+            ConnectToServer();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DisconnectButtonClick(object sender, EventArgs e)
+        {
+            DisconnectFromServer();
+        }
+    }
+}
