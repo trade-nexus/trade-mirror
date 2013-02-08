618ce43296f1eb250877cd4c1fea8abd671fa020
Umer Aziz Malik <umer@aurorasolutions.org>
2013-02-09 01:01:15 +0500

    + dmeo client installer project added

---
+++ Auto FX Tools Trade Mirror - Demo/Installers/Client/AutoFxProfitsTradeMirror/InstallerHelpProject/InstallerHelperClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace InstallerHelpProject
+{
+    [RunInstaller(true)]
+    public partial class InstallerHelperClass : System.Configuration.Install.Installer
+    {
+        private const string EnvironementVariable = "USERPROFILE";
+        public InstallerHelperClass()
+        {
+            InitializeComponent();
+
+            string appDataLocation = System.Environment.GetEnvironmentVariable("APPDATA");
+            Directory.CreateDirectory(appDataLocation + "\\AutoFX Profits Demo");
+            Directory.CreateDirectory(appDataLocation + "\\AutoFX Profits Demo\\AutoFXProfitsClientTerminal");
+            Directory.CreateDirectory(appDataLocation + "\\AutoFX Profits Demo\\logs");
+        }
+
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            string targeted = this.Context.Parameters["targetdir"];
+
+            DirFinder dr = new DirFinder();
+
+            dr.GetInstalledSoftware2(targeted);    
+        }
+
+    }
+}
