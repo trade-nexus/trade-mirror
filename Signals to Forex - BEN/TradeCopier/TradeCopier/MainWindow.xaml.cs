8db8a6e657d12528f7b5a5cf402a95a180688d10
Ahmad Masood <Ahmad Masood@AhmadMasood-PC.Home>
2013-02-26 11:31:23 +0500

    + Project added

---
+++ Signals to Forex - BEN/TradeCopier/TradeCopier/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TradeCopier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MyViewModel _vm ;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _vm = new MyViewModel();
+            this.DataContext = _vm;
+        }
+
+        private void Add_User_Click(object sender, RoutedEventArgs e)
+        {
+            Window addUser = new AddUser();
+            addUser.Show();
+        }
+
+        private void Click_Browse(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+        }
+
+        private void EditUser(object sender, RoutedEventArgs e)
+        {
+            Window editUser = new EditUser();
+            editUser.Show();
+            editUser.DataContext = _vm.CurrentUser;
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Signals to Forex - BEN/TradeCopier/TradeCopier/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace TradeCopier
+{
+    public class User : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { if (value != _id) { _id = value; OnPropertyChanged("Id"); } }
+        }
+
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set { { if (value != _userName) { _userName = value; OnPropertyChanged("UserName"); } } }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { { if (value != _password) { _password = value; OnPropertyChanged("Password"); } } }
+        }
+
+        private DateTime _created;
+        public DateTime Created
+        {
+            get { return _created; }
+            set { { if (value != _created) { _created = value; OnPropertyChanged("Created"); } } }
+        }
+
+        private bool _status;
+        public bool Status
+        {
+            get { return _status; }
+            set { { if (value != _status) { _status = value; OnPropertyChanged("Status"); } } }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { { if (value != _email) { _email = value; OnPropertyChanged("Email"); } } }
+        }
+        private void OnPropertyChanged(String stringName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(stringName));
+            }
+        }
+
+    }
+}
