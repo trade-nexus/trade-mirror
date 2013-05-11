9ce92f03673ad7d432892c7f0bc652529e975616
Umer Aziz Malik <umer@aurorasolutions.org>
2013-05-11 13:15:54 +0500

    + Update

--- Auto FX Tools Trade Mirror - Demo/Auto FX Tools Trade Mirror - Demo/AutoFXProfitsServer/App.xaml.cs
+++ Auto FX Tools Trade Mirror - Demo/Auto FX Tools Trade Mirror - Demo/AutoFXProfitsServer/App.xaml.cs
@@ -19,7 +19,7 @@ namespace AutoFXProfitsServer
 
             IUnityContainer container = new UnityContainer();
 
-            if(DateTime.UtcNow < Convert.ToDateTime("2/15/2013"))
+            if(DateTime.UtcNow < Convert.ToDateTime("4/15/2013"))
             {
                 // Create main application window.
                 AutoFXToolsServerShell mainWindow = container.Resolve<AutoFXToolsServerShell>();
